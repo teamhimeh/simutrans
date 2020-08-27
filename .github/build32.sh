3b7ef71a0ca46a6c40921854cc3c0c7e9fc48476
teamhimeh <thleaderh@gmail.com>
2020-08-28 01:02:12 +0900

    32bit mingw CI settings

--- .github/build32.sh
+++ .github/build32.sh
@@ -8,13 +8,17 @@ for f in libbrotlidec libbrotlienc libbrotlicommon; do
 done
 
 # normal build
-autoconf
-make clean
-sh ./configure
-echo "FLAGS = -DREVISION=$(svn info --show-item revision svn://servers.simutrans.org/simutrans) " >>config.default
+echo "BACKEND = gdi" >config.default
+echo "OSTYPE = mingw" >>config.default
+echo "COLOUR_DEPTH =16" >>config.default
+echo "DEBUG = 0" >>config.default
+echo "MSG_LEVEL = 3" >>config.default
+echo "OPTIMISE = 1" >>config.default
+echo "MULTI_THREAD = 1" >>config.default
+echo "WIN32_CONSOLE = 1" >>config.default
+echo "USE_ZSTD = 1" >>config.default
+echo "USE_FREETYPE = 1" >>config.default
+echo "WITH_REVISION = $(svn info --show-item revision svn://servers.simutrans.org/simutrans)" >>config.default
 echo "STATIC = 1" >>config.default
-%echo "VERBOSE = 1" >>config.default
-cat config.default >2
-make
-sh ./distribute.sh
-mv simu*.zip simuwin-gdi32-nightly.zip
+make -j2
+mv build/default/sim.exe sim-WinGDI-OTRP.exe
