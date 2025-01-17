fd236179e19309ec48ecef9ea8921808630b6352
simupoppo <simupoppo@gmail.com>
2025-01-18 00:04:41 +0900

    Fix: the coupling_done flag reset when next stop button pressed

--- simconvoi.cc
+++ simconvoi.cc
@@ -5390,6 +5390,9 @@ void convoi_t::next_stop_button_pressed() {
 	if(  self->is_coupled()  ) {
 		return;
 	}
+	if( self->is_coupling_done() ) {
+		self->set_coupling_done(false);
+	}
 	convoihandle_t c = self;
 	while( c.is_bound() ) {
 		schedule_t *schedule = c->get_schedule();
