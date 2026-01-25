f5f832f638c9293f1a1e2f2aa1168de76785e88f
danivenk <danivenk@gmail.com>
2026-01-25 01:11:31 +0100

    synchronize code with teamhimeh:OTRP-KUTAv6

--- gui/schedule_list.h
+++ gui/schedule_list.h
@@ -35,7 +35,6 @@ private:
 
 	button_t bt_new_line, bt_edit_line, bt_delete_line, bt_withdraw_line, bt_teleport_line_to_depot;
 	button_t bt_copy_line, bt_show_journey_time, bt_goods_waiting_time;
-	button_t bt_copy_data;
 	gui_scrolled_list_t scl, scrolly_convois, scrolly_haltestellen;
 	gui_speedbar_t filled_bar;
 	gui_textinput_t inp_name, inp_filter, inp_memo;
@@ -81,9 +80,6 @@ private:
 
 	uint8 current_sort_mode;
 	void build_line_list(int filter);
-
-	// copy data as csv format	
-	void copy_csv_format();
 
 public:
 	/// last selected line per tab
