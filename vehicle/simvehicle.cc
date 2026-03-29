cda7ee71e54895a7ddbce6d5f709d5a4f99d3697
simu_poppo <simupoppo@gmail.com>
2026-03-29 17:06:29 +0900

    add start_signal flag and button

--- vehicle/simvehicle.cc
+++ vehicle/simvehicle.cc
@@ -4165,7 +4165,14 @@ bool rail_vehicle_t::can_enter_tile(const grund_t *gr, sint32 &restart_speed, ui
 						restart_speed = 0;
 						return false;
 					}
-					if(  next_signal<(cnv->get_entire_convoy_length()/8+1)  ) {
+					if(  next_signal>cnv->get_route()->get_count()-1  ) {
+						// no signal until next stop, go!
+						cnv->set_next_stop_index(next_crossing < next_signal ? next_crossing : next_signal);
+						return true;
+					}
+					grund_t *gr_next_signal = welt->lookup(cnv->get_route()->at(next_signal));
+					signal_t *sig = gr_next_signal->find<signal_t>();
+					if(  sig!=NULL && sig->is_start_signal()  ) {
 						if(  is_signal_clear(next_signal, restart_speed, true)  ) {
 							// ok we start
 							return true;
@@ -4179,7 +4186,7 @@ bool rail_vehicle_t::can_enter_tile(const grund_t *gr, sint32 &restart_speed, ui
 					cnv->set_next_stop_index(next_crossing < next_signal ? next_crossing : next_signal);
 					return true;
 				}
-				else if(w->has_signal()) {
+				else if(w->has_signal()&&gr_current->find<signal_t>()->is_start_signal()) {
 					return is_signal_clear(max(route_index, 1) - 1, restart_speed);
 				}
 			} else if(  can_couple(cnv->get_route(), route_index, next_coupling, next_c_steps)  &&  next_coupling!=route_t::INVALID_INDEX  ) {
