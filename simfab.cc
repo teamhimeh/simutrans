250729eb293bdc38a78b992a959223c45303e0a0
simu_poppo <simupoppo@gmail.com>
2026-04-18 13:54:57 +0900

    update power supply calclation

--- simfab.cc
+++ simfab.cc
@@ -1996,10 +1996,6 @@ void fabrik_t::step(uint32 delta_t)
 							// if less than 3/4 filled we neary always consume power
 							currently_requiring_power |= (output[product].menge*4 < output[product].max*3);
 							currently_producing = true;
-							if(  desc->is_electricity_producer()  ) {
-								// power station => produce power
-								power += (uint32)( ((sint64)scaled_electric_demand * (sint64)(DEFAULT_PRODUCTION_FACTOR + prodfactor_pax + prodfactor_mail)) >> DEFAULT_PRODUCTION_FACTOR_BITS );
-							}
 						}
 						else {
 							output[product].book_stat((sint64)(output[product].max - 1 - output[product].menge) * (sint64)desc->get_product(product)->get_factor(), FAB_GOODS_PRODUCED);
@@ -2007,7 +2003,11 @@ void fabrik_t::step(uint32 delta_t)
 						}
 					}
 				}
-				set_power_supply(power);
+				if(  desc->is_electricity_producer()  ) {
+					// compute power production
+					uint64 pp = ((uint64)scaled_electric_demand * (uint64)boost * (uint64)work) >> (DEFAULT_PRODUCTION_FACTOR_BITS + WORK_BITS);
+					set_power_supply((uint32)pp);
+				}
 
 				break;
 			}
@@ -2107,7 +2107,6 @@ void fabrik_t::step(uint32 delta_t)
 							}
 						}
 					}
-					uint32 power = 0;
 
 					// and finally consume stock
 					for(  uint32 index = 0;  index < input.get_count();  index++  ) {
@@ -2116,10 +2115,6 @@ void fabrik_t::step(uint32 delta_t)
 						if(  (uint32)input[index].menge > v + 1  ) {
 							input[index].menge -= v;
 							input[index].book_stat((sint64)v * (sint64)desc->get_supplier(index)->get_consumption(), FAB_GOODS_CONSUMED);
-							if(  desc->is_electricity_producer()  ) {
-								// power station => produce power
-								power += (uint32)( ((sint64)scaled_electric_demand * (sint64)(DEFAULT_PRODUCTION_FACTOR + prodfactor_pax + prodfactor_mail)) >> DEFAULT_PRODUCTION_FACTOR_BITS );
-							}
 						}
 						else {
 							input[index].book_stat((sint64)input[index].menge * (sint64)desc->get_supplier(index)->get_consumption(), FAB_GOODS_CONSUMED);
@@ -2129,7 +2124,11 @@ void fabrik_t::step(uint32 delta_t)
 
 					// work done is consumption rate
 					work = work_from_production(prod, consumed_menge);
-					set_power_supply(power);
+					if(  desc->is_electricity_producer()  ) {
+						// compute power production
+						uint64 pp = ((uint64)scaled_electric_demand * (uint64)boost * (uint64)work) >> (DEFAULT_PRODUCTION_FACTOR_BITS + WORK_BITS);
+						set_power_supply((uint32)pp);
+					}
 				}
 
 				break;
