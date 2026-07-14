52c2b39443cbfdbe47b39e372198a352fe81df5c
simu_poppo <simupoppo@gmail.com>
2026-07-15 00:32:32 +0200

    rdwr update for unowned player

--- obj/simobj.cc
+++ obj/simobj.cc
@@ -185,8 +185,16 @@ void obj_t::rdwr(loadsave_t *file)
 	file->rdwr_byte(byte);
 	yoff = (sint8)(((sint16)byte*OBJECT_OFFSET_STEPS)/16);
 	byte = owner_n;
+	// in old version, unowned player id is 15.
+	if(  file->is_saving()  &&  file->get_OTRP_version()<58  &&  byte==PLAYER_UNOWNED  ) {
+		byte = 15;
+	}
 	file->rdwr_byte(byte);
-	owner_n = byte;
+	if(  file->is_loading()  &&  file->get_OTRP_version()<58  &&  byte==15  ) {
+		owner_n = PLAYER_UNOWNED;
+	} else {
+		owner_n = byte;
+	}
 }
 
 
