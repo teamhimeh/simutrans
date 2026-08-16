e23f7c4948e610f7f4803fef1f95a7fb7bbbb02f
simu_poppo <simupoppo@gmail.com>
2026-08-17 00:25:37 +0900

    space setting update

--- gui/schedule_gui.cc
+++ gui/schedule_gui.cc
@@ -542,15 +542,13 @@ void schedule_gui_t::init(schedule_t* schedule_, player_t* player, convoihandle_
 		bt_reverse_default.pressed = schedule->is_reverse_default();
 		bt_reverse_default.add_listener(this);
 		add_component(&bt_reverse_default);
-		add_component(&sp_schedule_reverse_settings);
-		add_component(&sp_schedule_reverse_settings);
+		add_component(&sp_schedule_reverse_settings,2);
 
 		bt_no_use_electric.init(button_t::square_state, "Not use electricity");
 		bt_no_use_electric.set_tooltip(translator::translate("Not use electricity in this schedule"));
 		bt_no_use_electric.add_listener(this);
 		add_component(&bt_no_use_electric);
-		add_component(&sp_coupling_settings);
-		add_component(&sp_coupling_settings);
+		add_component(&sp_coupling_settings,2);
 	}
 	end_table();
 
